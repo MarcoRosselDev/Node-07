24ef1118b2f533d946aad351b8bd00edf6f3f536
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-27 05:12:54 -0300

    basic express

--- connect.js
+++ connect.js
@@ -1,6 +1,8 @@
 // Using Node.js `require()`
 const mongoose = require('mongoose');
 
-const connect = mongoose.connect('mongodb://127.0.0.1:27017/test')
+const connect = async (url) =>{
+  return await mongoose.connect(url)
+}
 
-module.exports = mongoose.connect('mongodb://127.0.0.1:27017/test')
+module.exports = connect;
--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-require('dotenv').config()
-console.log(process.env.MONGO_URL) // remove this after you've confirmed it is working
-
 const connect = require('./connect.js')
+require('dotenv').config()
+// express
+const express = require('express');
+const app = express()
+const puerto = process.env.PUERTO;
 
-connect.then(()=> console.log('hi'));
+app.get('/', (req, res) => {
+  res.status(200).json({msj: 'Hola mundo!'})
+})
+
+
+const start = async () =>{
+  try {
+    await connect(process.env.MONGO_URL)
+    app.listen(puerto, ()=>{
+      console.log(`Escuchando en el puerto ${puerto}`);
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+start()
