29686b6f1c820512b97635ff2c17268bd2fd9880
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-29 13:57:03 -0300

    working just with vanilla js and vanilla css

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 const start = require('./start.js')
 const morgan = require('morgan'); //--> log info about method, status, time, etcGIT 
-const {join} = require('path')
-const exphbs = require("express-handlebars");
+const path = require('path');
 // express
 const express = require('express');
 const app = express()
 // router
 const user = require('./router/usuario.js')
 
-// config view engine
-const hbs = exphbs.create({
-  defaultLayout: "main",
-  layoutsDir: join(app.get("views"), "layouts"),
-  partialsDir: join(app.get("views"), "partials"),
-  extname: ".hbs",
-});
-app.engine(".hbs", hbs.engine);
-app.set("view engine", ".hbs");
-
 //express settings
 app.use(express.urlencoded({extended: false}))// decodificar json aplication
-//app.use(express.json())// ---> req.body --> si no, no se envia el body
+app.use(express.json())// ---> req.body --> si no, no se envia el body
 app.use(morgan('dev')) // ---> log() info sobre la peticion, estado, method, etc
-//app.use('/', user)
+app.use('/', user)
+
+//carpeta public
+app.use(express.static(path.join(__dirname, 'public')))
+// por que usar path.join?
+// para escribir rutas en wind o linux es distinto = .folder/a != .folder\a
+// cosa que al momento de desplegar en el servidor nos puede dar problemas.
+// por eso aplicamos path.join que une la ruta asta el proyecto __dirname + public
 
 //test get
 app.get('/login', (req, res) =>{
