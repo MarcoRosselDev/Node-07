75f62f851091b18dd86ed4d4981a09938c3dc38d
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-29 14:45:43 -0300

    working just with vanilla js and vanilla css 2

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
-const start = require('./start.js')
 const morgan = require('morgan'); //--> log info about method, status, time, etcGIT 
 const path = require('path');
+
+const connect = require('./connect.js');
+require('dotenv').config();
+const url = process.env.MONGO_URL;
+const puerto = process.env.PUERTO || 3000;
 // express
 const express = require('express');
 const app = express()
 // router
 const user = require('./router/usuario.js')
 
-//express settings
 app.use(express.urlencoded({extended: false}))// decodificar json aplication
 app.use(express.json())// ---> req.body --> si no, no se envia el body
-app.use(morgan('dev')) // ---> log() info sobre la peticion, estado, method, etc
+
+//express settings
+//app.use(morgan('dev')) // ---> log() info sobre la peticion, estado, method, etc
 app.use('/', user)
 
-//carpeta public
 app.use(express.static(path.join(__dirname, 'public')))
+//carpeta public
 // por que usar path.join?
 // para escribir rutas en wind o linux es distinto = .folder/a != .folder\a
 // cosa que al momento de desplegar en el servidor nos puede dar problemas.
 // por eso aplicamos path.join que une la ruta asta el proyecto __dirname + public
 
-//test get
-app.get('/login', (req, res) =>{
-  res.render('index')
-})
+const start = async () => {
+  try {
+    await connect(url);
+    app.listen(puerto, () => {
+      console.log(`Escuchando en el puerto ${puerto}`);
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 start()
