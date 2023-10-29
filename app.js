451dbabcc35be0399dab2a18f70f990c7e020df5
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-29 09:22:38 -0300

    andlebars engine

--- app.js
+++ app.js
@@ -8,15 +8,20 @@ const app = express()
 // router
 const user = require('./router/usuario.js')
 
-//handlebars settings
-app.engine('handlebars', engine())
+//express settings
 app.use(express.urlencoded({extended: false}))// decodificar json aplication
 app.use(express.json())// ---> req.body --> si no, no se envia el body
 app.use(morgan('dev')) // ---> log() info sobre la peticion, estado, method, etc
 app.use('/', user)
-app.get('/login', (req, res)=>{
-  res.json({data: 'hola'})
-})
+
+//handlebars settings
+app.engine('.hbs', engine({
+  defaultLayout: 'main',
+  layoutDir: path.join( app.get('views'), 'layouts'),
+  partialsDir: path.join( app.get('views'), 'partials'),
+  extname: '.hbs'
+}))
+app.set('view engine', '.hbs')
 // carpeta de archivos estaticos
 //app.use(express.static(path.join(__dirname, 'public')))
 // conect mondodb y app.listen
