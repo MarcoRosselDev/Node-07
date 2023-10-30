8443f1b0667b1dfa2e74cc718c774215983040b3
Marco Rossel <andresmarcorossel@gmail.com>
2023-10-30 16:15:24 -0300

    registrar 04 vanilla

--- public/public.js
+++ public/public.js
@@ -1,1 +1,7 @@
-console.log('Hola mundo');
+const hamburguesa = document.querySelector('.hamburguesa');
+
+hamburguesa.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log('click Hamburguesa');
+  console.log(this.parentNode);
+})
