7b0f217cb85459650387aa74a18b00154847dd55
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-01 17:07:12 -0300

    login 03 vanilla

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,1 +1,14 @@
-console.log('login testing script');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const submit = document.getElementById('sendLogin');
+
+submit.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log(email.value);
+  console.log(password.value);
+  // enviar un fetch con los valore,
+  // encriptar el password para compararlo con el password de la database
+  // si coinsiden los valores generar un token con jwt
+  // buscar como guardar el jwt en el seccion storage y local storage
+  // pista, usar cookies
+})
