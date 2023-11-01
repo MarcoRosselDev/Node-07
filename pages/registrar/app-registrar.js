aa162be6e8333b0ad4ffb046d1c7e862697f2f77
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-01 07:46:06 -0300

    registrar 16 vanilla

--- pages/registrar/app-registrar.js
+++ pages/registrar/app-registrar.js
@@ -1,6 +1,14 @@
 const enviar = document.getElementById('enviar');
+const nombre = document.getElementById('usuario');
+const password = document.getElementById('password');
+const passwordComparativo = document.getElementById('password-comparativo');
+const email = document.getElementById('email');
 
 enviar.addEventListener('click', function (e) {
   e.preventDefault();
   console.log('click en enviar!');
+  console.log(nombre.value);
+  console.log(email.value);
+  console.log(password.value);
+  console.log(passwordComparativo.value);
 })
