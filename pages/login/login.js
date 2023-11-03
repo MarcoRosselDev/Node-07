8bd41742814af8033d6247b8656eac0d85d20dd0
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-02 23:30:55 -0300

    tarea 04 vanilla | jwt successful

--- pages/login/login.js
+++ pages/login/login.js
@@ -2,13 +2,30 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const submit = document.getElementById('sendLogin');
 
-submit.addEventListener('click', function (e) {
+submit.addEventListener('click', async function (e) {
   e.preventDefault();
   console.log(email.value);
   console.log(password.value);
-  // enviar un fetch con los valore,
-  // encriptar el password para compararlo con el password de la database
-  // si coinsiden los valores generar un token con jwt
-  // buscar como guardar el jwt en el seccion storage y local storage
-  // pista, usar cookies
+  try {
+    const login = await fetch('/api/v1/login', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify({
+        email: email.value,
+        password: password.value
+      })
+    });
+
+    console.log(login);
+    /* login
+      .then(data => data.json())
+      .then(valor => console.log(valor)) */
+  } catch (error) {
+    console.log(error);
+  }
 })
+
+console.log(document.cookie);
