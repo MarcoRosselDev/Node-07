dee14b4bdef73424829a337864d0cbc424a26cda
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-04 19:40:57 -0300

    cookies 01 vanilla

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-const morgan = require('morgan'); //--> log info about method, status, time, etcGIT 
-const path = require('path');
-
-const connect = require('./connect.js');
 require('dotenv').config();
+const connect = require('./connect.js');
 const url = process.env.MONGO_URL;
+const path = require('path');
+const morgan = require('morgan'); //--> log info about method, status, time, etcGIT 
 const puerto = process.env.PUERTO || 3000;
-// express
-const express = require('express');
-const app = express()
-// router
+
 const user = require('./router/ruta-usuario.js')
 
-app.use(express.urlencoded({extended: false}))// decodificar json aplication
-app.use(express.json())// ---> req.body --> si no, no se envia el body
+// express
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const app = express();
 
 //express settings
-app.use(morgan('dev')) // ---> log() info sobre la peticion, estado, method, etc
-app.use('/api/v1', user)
-app.use(express.static(path.join(__dirname, 'public')))
-app.use('/login', express.static(path.join(__dirname, 'pages', 'login')))
-app.use('/qwe', express.static(path.join(__dirname, 'pages', 'home')))
-app.use('/registrar', express.static(path.join(__dirname, 'pages', 'registrar')))
+app.use(express.urlencoded({extended: false}));// decodificar json aplication
+app.use(express.json());// ---> req.body --> si no, no se envia el body
+app.use(cookieParser());
 
+app.use(morgan('dev')) // ---> log() info sobre la peticion, estado, method, etc
+
+app.use('/api/v1', user) // ---> router
+app.use(express.static(path.join(__dirname, 'public')));//---> url "/"
+app.use('/login', express.static(path.join(__dirname, 'pages', 'login')))
+app.use('/home', express.static(path.join(__dirname, 'pages', 'home')))
+app.use('/registrar', express.static(path.join(__dirname, 'pages', 'registrar')))
 
 const start = async () => {
   try {
@@ -34,4 +36,4 @@ const start = async () => {
   }
 }
 
-start()
+start();
