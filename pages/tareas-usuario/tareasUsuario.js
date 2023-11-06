82619259ff57a5fac1018723274802ae63773ee1
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-06 14:10:49 -0300

    cookies 05 vanilla

--- pages/tareas-usuario/tareasUsuario.js
+++ pages/tareas-usuario/tareasUsuario.js
@@ -1,2 +1,15 @@
 console.log('tareas usuario script');
 console.log(document.cookie);
+
+try {
+  const response = fetch('/api/v1/tarea', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  })
+  console.log(response);
+} catch (error) {
+  console.log(error);
+}
