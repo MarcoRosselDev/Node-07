020b6219b239b26e0517e1af0acc618d9f74cc48
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-06 21:03:56 -0300

    v2 | testing middlewares 2

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const tareas = require('./router/tareas.js');
 
+//express settings
+app.use(express.urlencoded({extended: false}));// decodificar json aplication
+app.use(express.json());// ---> req.body --> si no, no se envia el body
+
 app.use('/api/v2', tareas);
 app.get('/', (req, res) => {
   res.send('Hola mundo!')
--- router/tareas.js
+++ router/tareas.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const router = express.Router();
-console.log(router);
 
 //middleware testing
-router.use(function (req, res, next) {
+const testMiddleware = (req, res, next) =>{
+  req.nombre = 'Marco'
+  console.log('Hola desde test Middleware fn');
+  console.log('puede ir alguna loginca y ejecucion');
+  next()
+}
+/* router.use(function (req, res, next) {
   console.log("Tiempo", Date.now());
   console.log('Porbando middleware en router')
   next()
-})
+}) */
 
-router.get('/', (req, res) =>{
-  res.send('ruta / desde router')
+const usarNombre = (req, res, next) =>{
+  req.saludo = `Hola ${req.nombre}, buenos dias!`
+  next()
+}
+
+router.get('/', testMiddleware, usarNombre, (req, res) =>{
+  const final = `log ==> ${req.saludo}`;
+  res.send(`${final}, desde '/' de router tareas`)
 })
 
 module.exports = router;
